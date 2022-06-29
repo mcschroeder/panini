c58bebd4fce0f184fb1e0c231c3ba928d2640536
Michael Schröder <michael.schroeder@tuwien.ac.at>
2022-06-29 17:20:32 +0200

    Document the FUSION algorithm

--- src/Panini/Solver/Fusion.hs
+++ src/Panini/Solver/Fusion.hs
@@ -35,20 +35,27 @@ sat c q = do
       return True
     Nothing -> return False
 
+-- | Eliminates a set of acyclic κ-variables iteratively via 'elim1'.
 elim :: [KVar] -> Con -> Con
 elim []     c = c
 elim (k:ks) c = elim ks (elim1 k c)
 
+-- | Eliminates κ from a constraint c by invoking 'elim'' on the strongest
+-- scoped solution for κ in c.
 elim1 :: KVar -> Con -> Con
-elim1 k c = {- trace (prettyAss sk) $ -} elim' sk c
+elim1 k c = elim' sk c
   where
     sk = Map.singleton k (kparams k, sol1 k c')
-    c' = flatHead2 $ scope k c
+    c' = skipHypos $ scope k c
 
-flatHead2 :: Con -> Con
-flatHead2 (CAll _ _ _ c) = flatHead2 c
-flatHead2 c = c
+-- | Given a constraint of the form ∀x₁:b₁. p₁ ⇒ … ⇒ ∀xₙ:bₙ. pₙ ⇒ c',
+-- @skipHypos@ will omit the hypotheses p₁,…,pₙ and return just c'. 
+skipHypos :: Con -> Con
+skipHypos (CAll _ _ _ c) = skipHypos c
+skipHypos c              = c
 
+-- | @sol1 κ c@ returns a predicate that is guaranteed to satisfy all clauses in
+-- c where κ appears as the head.
 sol1 :: KVar -> Con -> Pred
 sol1 k (CAnd c1 c2)   = (sol1 k c1) `pOr` (sol1 k c2)
 sol1 k (CAll x b p c) = PExists x b (p `pAnd` sol1 k c)
@@ -57,14 +64,20 @@ sol1 k (CHead (PAppK k2 ys))
                              $ zip (kparams k) ys
 sol1 _ _              = PFalse NoPV
 
+-- | @scope κ c@ returns a sub-constraint of c of the form ∀x₁:b₁. p₁ ⇒ … ⇒
+-- ∀xₙ:bₙ. pₙ ⇒ c' such that κ does not occur in p₁,…,pₙ, and all occurrences of
+-- κ in c occur in c'. Any solution of κ can omit the hypotheses p₁,…,pₙ because
+-- they are already present at all /uses/ of κ.
 scope :: KVar -> Con -> Con
 scope k (CAnd c1 c2)
   | k    `elem` kvars c1, k `notElem` kvars c2 = scope k c1
   | k `notElem` kvars c1, k    `elem` kvars c2 = scope k c2
 scope k (CAll x b p c')
-  | k `notElem` kvars p                        = CAll x b p (scope k c')
+  | k `notElem` kvars p                        = CAll x b p $ scope k c'
 scope _ c                                      = c
 
+-- | Eliminates all κ-variables from a constraint by replacing each
+-- body-occurrence of a κ by σ(κ) and each head-occurrence of a κ by /true./
 elim' :: Assignment -> Con -> Con
 elim' s (CAnd c1 c2)   = elim' s c1 `cAnd` elim' s c2
 elim' s (CAll x b p c) = CAll x b (apply s p) (elim' s c)
--- src/Panini/Syntax/AST.hs
+++ src/Panini/Syntax/AST.hs
@@ -160,8 +160,12 @@ pOr _          (PTrue pv) = PTrue pv
 pOr p          q          = PDisj p q
 
 ------------------------------------------------------------------------------
--- Horn constraints
+-- Constraints
 
+-- | Constraints are Horn clauses in negation normal form (NNF). They form a
+-- tree, where each leaf is a goal ('CHead') and each node either quantifies
+-- some variable to satisfy a hypothesis ('CAll') or conjoins two
+-- sub-constraints ('CAnd').
 data Con
   = CHead Pred               -- p
   | CAnd Con Con             -- c1 /\ c2
