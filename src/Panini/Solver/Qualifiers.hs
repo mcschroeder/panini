48f72495e16c94b2bc55fa5531d06ffbd7203448
Michael Schröder <michael.schroeder@tuwien.ac.at>
2024-01-24 11:05:27 +0100

    Generalize singular qualifier extraction

--- src/Panini/Solver/Qualifiers.hs
+++ src/Panini/Solver/Qualifiers.hs
@@ -20,14 +20,23 @@ extractQualifiers con =
 
 quals :: Con -> [(Int,Base)] -> [Pred]
 quals con = \case  
-  [(_,TUnit)] -> [PTrue]
-  [(i,TBool)] -> [ PRel $ Rel op (z i) e2 | PRel (Rel op (EVar _) e2@(EBool _ _)) <- universeBi con ]
-  [(i,TInt)] -> [ PRel $ Rel op (z i) e2 | PRel (Rel op (EVar _) e2@(EInt _ _)) <- universeBi con ]
-  [(i,TInt),(j,TInt)] ->  [ PRel $ substN [z i, z j] xs r
+  [(_, TUnit)] -> [PTrue]
+  [(i, b0)]    -> [PRel r | PRel (q1 (z i, b0) -> Just r) <- universeBi con]
+
+  -- TODO: make type safe
+  -- TODO: generalize
+  [(i,TInt),(j,TInt)] ->  [ PRel $ substN [EVar (z i), EVar (z j)] xs r
                   | PRel r <- universeBi con 
                   , let fvs = freeVars r, Set.size fvs == 2
                   , xs <- List.permutations $ Set.toList fvs
                   ]
   _ -> []
  where
-  z i = EVar $ Name ("z" <> Text.pack (show i)) NoPV
+  z i = Name ("z" <> Text.pack (show i)) NoPV
+
+q1 :: (Name,Base) -> Rel -> Maybe Rel
+q1 (z0,b0) = \case
+  Rel op (EVar _) e | typeOfExpr e == Just b0 -> Just $ Rel op (EVar z0) e
+  r | Just (Rel op (EVar _) e) <- converse r
+    , typeOfExpr e == Just b0 -> Just $ Rel op (EVar z0) e
+  _ -> Nothing
