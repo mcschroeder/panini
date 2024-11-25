4eeac8b000ca5be8f348bdfc26373bdac67f4426
Michael Schröder <michael.schroeder@tuwien.ac.at>
2024-11-25 19:25:02 +0100

    Rewrite regex eval to compute summary stats

--- tests/regex/eval_simplify.hs
+++ tests/regex/eval_simplify.hs
@@ -1,51 +1,219 @@
 {-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE ViewPatterns #-}
 
 import Control.Monad
+import Control.Monad.Combinators.NonEmpty qualified as NE
+import Data.Foldable
+import Data.IORef
 import Data.List (isInfixOf)
+import Data.List.NonEmpty qualified as NE
 import Data.Maybe
+import Data.Vector qualified as V
+import Data.Vector.Mutable qualified as VM
+import Data.Vector.Unboxed.Mutable qualified as VUM
+import Data.Void
+import Panini.Panic
+import Panini.Pretty
 import Panini.Pretty
 import Panini.Regex
 import Panini.Regex.POSIX.BE as BE
 import Panini.Regex.POSIX.ERE as ERE
 import Panini.Regex.Simplify
 import Panini.Regex.Simplify.Common
-import Prelude
+import Prelude hiding (minimum, maximum)
+import Statistics.Quantile
+import Statistics.Sample
 import System.Clock
 import System.Directory.Extra
+import System.Environment
 import System.FilePath
 import System.IO
 import System.Time.Extra
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer qualified as L
 import Text.Printf
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  putStrLn "file,line,size_before,size_after,time_ns,result"
-  inputFiles <- gatherInputFiles
+
+  args <- getArgs
+  let args' = filter (/= "--verify") args
+  let verify = length args /= length args'
+  when verify $ putStrLn "[verification enabled]"
+
+  inputFiles <- gatherInputFiles args'
+
+  putStrLn ""
+  putStrLn "                                         time (ms)             output size"
+  putStrLn "                               ┌───────────────────────────┬──────────────────┐"
+  putStrLn "                        ✓    - │  median     mean       sd │ ×minimum  ×input │"
+  putStrLn "───────────────────────────────┼───────────────────────────┼──────────────────┤"
+
+  numOkTotalRef <- newIORef (0 :: Int)
+  numTimeoutsTotalRef <- newIORef (0 :: Int)
+  timesTotalRef <- newIORef []
+  sizeExcessTotalRef <- newIORef []
+  sizeShrinkageTotalRef <- newIORef []
+  timesOkTotalRef <- newIORef []
+  sizeExcessOkTotalRef <- newIORef []
+  sizeShrinkageOkTotalRef <- newIORef []
+
   forM_ inputFiles $ \inputFile -> do
     let fileName = takeFileName inputFile
     rawLines <- lines <$> readFile inputFile
-    forM_ (zip rawLines [1..]) $ \(raw, i :: Int) -> do
-      printf "%s,%d," fileName i      
-      -- convert MrE regex format to POSIX
-      let raw' | "MrE" `isInfixOf` inputFile = map (\c -> case c of '+' -> '|'; _ -> c) raw
-               | otherwise = raw
-      let pos1 = fromJust $ ERE.parseERE raw'
-      let !reg1 = ERE.toRegex pos1
-      let len1 = size reg1
-      printf "%d," len1
-      t1 <- getTime Monotonic
-      !result <- timeout 5 $ return $! simplify reg1
-      t2 <- getTime Monotonic
-      let delta = toNanoSecs $ diffTimeSpec t1 t2
-      case result of
-        Nothing -> printf "%d,%d,timeout\n" len1 delta
-        Just reg2 -> do
-          let len2 = size reg2
-          printf "%d,%d," len2 delta
-          case equivalence reg1 reg2 of
-            True -> printf "ok\n"
-            False -> printf "wrong\n"
+    let n = length rawLines
 
-gatherInputFiles :: IO [FilePath]
-gatherInputFiles = listFilesRecursive "tests/regex/data"
+    numTimeoutsRef <- newIORef (0 :: Int)
+    timesRef <- newIORef []
+    sizeExcessRef <- newIORef []
+    sizeShrinkageRef <- newIORef []
+
+    printf "%s" fileName
+
+    forM_ (zip rawLines [1..]) $ \(rawLine, i :: Int) -> do
+      let regexFormat = guessFormat inputFile
+      let !regBefore = parseRegex regexFormat rawLine
+      let !sizeBefore = size regBefore      
+      t1 <- getTime Monotonic
+      !result <- timeout 5 $ return $! simplify regBefore
+      t2 <- getTime Monotonic
+      let testTimeMillis = (toNanoSecs $ diffTimeSpec t1 t2) ./. 1000000
+      let !sizeAfter | Just regAfter <- result = size regAfter
+                     | otherwise = sizeBefore
+      let sizeMinimal = min sizeAfter <$> guessMinimalSize inputFile
+      let sizeExcess = maybe 0 (sizeAfter ./.) sizeMinimal
+      let sizeShrinkage = sizeAfter ./. sizeBefore
+
+      modifyIORef' timesRef (testTimeMillis:)
+      modifyIORef' sizeExcessRef (sizeExcess:)
+      modifyIORef' sizeShrinkageRef (sizeShrinkage:)
+
+      modifyIORef' timesTotalRef (testTimeMillis:)
+      modifyIORef' sizeExcessTotalRef (sizeExcess:)
+      modifyIORef' sizeShrinkageTotalRef (sizeShrinkage:)      
+
+      case result of
+        Just regAfter | verify, not $ equivalence regBefore regAfter -> 
+          error $ printf "unsound simplification for %s:%d" inputFile i
+
+        Just regAfter -> do
+          modifyIORef' numOkTotalRef (+1)
+          modifyIORef' timesOkTotalRef (testTimeMillis:)
+          modifyIORef' sizeExcessOkTotalRef (sizeExcess:)
+          modifyIORef' sizeShrinkageOkTotalRef (sizeShrinkage:)
+
+        Nothing -> do
+          let loc :: String = printf "%s:%d" fileName i
+          printf "\r%-30s ┆       -        -        - ┆        -       - ┆\n" loc
+          modifyIORef' numTimeoutsRef (+1)
+          modifyIORef' numTimeoutsTotalRef (+1)
+
+      times <- V.fromList <$> readIORef timesRef
+      sizeExcess <- V.fromList <$> readIORef sizeExcessRef
+      sizeShrinkage <- V.fromList <$> readIORef sizeShrinkageRef
+
+      let minTime = V.minimum times
+      let (meanTime, varTime) = meanVarianceUnb times
+      let sdTime = sqrt varTime
+      let medianTime = median s times
+      let maxTime = V.maximum times
+      
+      numTimeouts <- readIORef numTimeoutsRef
+      let numOk = i - numTimeouts
+      let geomeanExcess = geometricMean sizeExcess
+      let geomeanShrinkage = geometricMean sizeShrinkage 
+
+      printf "\r%-20s %4d %4d │ %7.2f  %7.2f  %7.2f │ %8.4f  %6.4f │" 
+        fileName numOk numTimeouts 
+        medianTime meanTime sdTime 
+        geomeanExcess geomeanShrinkage
+    
+    printf "\n"
+  
+  putStrLn "───────────────────────────────┼───────────────────────────┼──────────────────┤"
+
+  times <- V.fromList <$> readIORef timesTotalRef
+  sizeExcess <- V.fromList <$> readIORef sizeExcessTotalRef
+  sizeShrinkage <- V.fromList <$> readIORef sizeShrinkageTotalRef
+
+  let minTime = V.minimum times
+  let (meanTime, varTime) = meanVarianceUnb times
+  let sdTime = sqrt varTime
+  let medianTime = median s times
+  let maxTime = V.maximum times
+      
+  numTimeouts <- readIORef numTimeoutsTotalRef
+  numOk <- readIORef numOkTotalRef
+  let geomeanExcess = geometricMean sizeExcess
+  let geomeanShrinkage = geometricMean sizeShrinkage 
+
+  printf "%-19s %5d %4d │ %7.2f  %7.2f  %7.2f │ %8.4f  %6.4f │\n"
+    "total" numOk numTimeouts 
+    medianTime meanTime sdTime
+    geomeanExcess geomeanShrinkage
+
+  when (numTimeouts > 0) $ do
+    timesOk <- V.fromList <$> readIORef timesOkTotalRef
+    sizeExcessOk <- V.fromList <$> readIORef sizeExcessOkTotalRef
+    sizeShrinkageOk <- V.fromList <$> readIORef sizeShrinkageOkTotalRef
+
+    let minTimeOk = V.minimum timesOk
+    let (meanTimeOk, varTimeOk) = meanVarianceUnb timesOk
+    let sdTimeOk = sqrt varTimeOk
+    let medianTimeOk = median s timesOk
+    let maxTimeOk = V.maximum timesOk      
+    let geomeanExcessOk = geometricMean sizeExcessOk
+    let geomeanShrinkageOk = geometricMean sizeShrinkageOk
+
+    printf "%-19s %5d %4d │ %7.2f  %7.2f  %7.2f │ %8.4f  %6.4f │\n"
+      "total w/o timeouts" numOk (0 :: Int)
+      medianTimeOk meanTimeOk sdTimeOk
+      geomeanExcessOk geomeanShrinkageOk
+
+
+(./.) :: Integral a => a -> a -> Double
+a ./. b = fromIntegral a / fromIntegral b
+
+data TestStatus = Ok | Wrong | Timeout
+
+guessMinimalSize :: FilePath -> Maybe Int
+guessMinimalSize inputFile
+  | "expansions" `isInfixOf`inputFile
+  , ('s': (reads @Int -> [(s,_)])) <- takeFileName inputFile = Just s
+guessMinimalSize _ = Nothing
+
+data RegexFormat = MrE | POSIX
+
+guessFormat :: FilePath -> RegexFormat
+guessFormat inputFile 
+  | "MrE" `isInfixOf` inputFile = MrE
+  | otherwise = POSIX
+
+parseRegex :: RegexFormat -> String -> Regex
+parseRegex MrE   = ERE.toRegex . fromJust . parseMrE
+parseRegex POSIX = ERE.toRegex . fromJust . ERE.parseERE
+
+parseMrE :: String -> Maybe ERE
+parseMrE = parseMaybe @Void mre
+ where
+  mre :: (MonadParsec e s m, Token s ~ Char) => m ERE
+  mre = Alt <$> NE.sepBy1 con (char '+')
+   where
+    con = Con <$> NE.some exp
+    exp = do
+      e <- choice [chr, grp]
+      ds <- many $ choice [ast, que]
+      return $ foldl' Dup e ds
+    chr = Chr <$> satisfy (`notElem` ("+()*?" :: [Char]))
+    grp = Grp <$ char '(' <*> mre <* char ')'
+    ast = Ast <$ char '*'
+    que = Que <$ char '?'
+
+gatherInputFiles :: [String] -> IO [FilePath]
+gatherInputFiles [] = return []
+gatherInputFiles (x:xs) = do
+  isFile <- doesFileExist x
+  inputs <- if isFile then return [x] else listFilesRecursive x
+  (inputs ++) <$> gatherInputFiles xs
