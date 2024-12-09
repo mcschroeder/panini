a09bab153290ce9c95ed5aff99ed1da1dd713d3e
Michael Schröder <michael.schroeder@tuwien.ac.at>
2024-12-09 16:29:45 +0100

    Split off AssignKVars from HasKVars

--- src/Panini/Solver/Assignment.hs
+++ src/Panini/Solver/Assignment.hs
@@ -9,37 +9,52 @@ import Panini.Solver.Constraints
 import Panini.Syntax
 import Prelude
 
--- | An assignment σ maps κ-variables to refinement predicates.
-type Assignment = Map KVar Pred
+-------------------------------------------------------------------------------
 
 class HasKVars a where
   -- | The set of κ-variables in a constraint or predicate.
   kvars :: a -> Set KVar
 
+instance HasKVars Pred where
+  kvars p = Set.fromList [k | PAppK k _ <- universe p]
+
+instance HasKVars Con where
+  kvars c = foldMap kvars $ universeBi @Con @Pred c
+
+instance HasKVars FlatCon where
+  kvars (FAll _ p q) = kvars p <> kvars q
+
+instance HasKVars [FlatCon] where
+  kvars = foldMap kvars
+
+instance HasKVars Type where
+  kvars = foldMap kvars . universeBi @Type @Pred
+
+-------------------------------------------------------------------------------
+
+-- | An assignment σ maps κ-variables to refinement predicates.
+type Assignment = Map KVar Pred
+
+class AssignKVars a where
   -- | Replaces each κ-application κ(y̅) in a constraint or predicate with its
   -- solution σ(κ)[y̅/z̅] where z̅ = kparams(κ).
   apply :: Assignment -> a -> a
 
-instance HasKVars Pred where
-  kvars p = Set.fromList [k | PAppK k _ <- universe p]
+instance AssignKVars Pred where
   apply s = transform $ \case
     PAppK k ys -> case Map.lookup k s of
       Just p  -> substN ys (kparams k) p
       Nothing -> PAppK k ys
     p -> p
 
-instance HasKVars Con where
-  kvars c = foldMap kvars $ universeBi @Con @Pred  c
+instance AssignKVars Con where  
   apply s = transformBi @Con @Pred (apply s)
 
-instance HasKVars FlatCon where
-  kvars (FAll _ p q) = kvars p <> kvars q
+instance AssignKVars FlatCon where
   apply s (FAll xs p q) = FAll xs (apply s p) (apply s q)
 
-instance HasKVars [FlatCon] where
-  kvars = foldMap kvars
+instance AssignKVars [FlatCon] where
   apply s = fmap (apply s)
 
-instance HasKVars Type where
-  kvars = foldMap kvars . universeBi @Type @Pred
+instance AssignKVars Type where
   apply s = transformBi @Type @Pred (apply s)
