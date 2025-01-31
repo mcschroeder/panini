8cc5c19d923fa13e7dd8a4d4f50ac923da328f97
Michael Schröder <michael.schroeder@tuwien.ac.at>
2025-01-31 15:30:36 +0100

    Improve collating Stalagmite benchmark results

--- benchmarks/oopsla25/stalagmite/collate_results.sh
+++ benchmarks/oopsla25/stalagmite/collate_results.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Directory paths
 accuracy_dir="stalagmite/output/paper/accuracy/csv"
 time_file="stalagmite/output/paper/time.csv"
 output_file="results.csv"
 
-# Initialize the output CSV with the header
-echo "subject,precision,recall,time_s" > "$output_file"
+echo "subject,time_seconds,precision,recall" > "$output_file"
 
-for file in "$accuracy_dir"/accuracy_*.csv; do
-    subject=$(basename "$file" | sed 's/accuracy_\(.*\)\.csv/\1/')
-    read precision recall <<< $(awk -F, '$1=="refined" {print $2, $3}' "$file")
-    time_sec=$(awk -F, -v subject="$subject" '$1 == subject {print $2}' "$time_file")
-    echo "$subject,$precision,$recall,$time_sec" >> "$output_file"
-done
+while IFS=, read -r subject time_seconds; do
+  if [[ "$subject" == "subject" ]]; then
+    continue
+  fi
+  
+  accuracy_file="$accuracy_dir/accuracy_${subject}.csv"
+  if [[ -f "$accuracy_file" ]]; then
+    read precision recall <<< $(awk -F, '$1=="refined" {print $2, $3}' "$accuracy_file")
+  else
+    precision=0
+    recall=0
+  fi
+  echo "$subject,$time_seconds,$precision,$recall" >> "$output_file"
+
+done < "$time_file"
