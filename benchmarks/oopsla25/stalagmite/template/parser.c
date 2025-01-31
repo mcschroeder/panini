3103060db08ac6277541f8a8d76ce495204dd88a
Michael Schröder <michael.schroeder@tuwien.ac.at>
2025-01-31 13:46:54 +0100

    Fix Stalagmite benchmark subject scaffolding
    Ensure all input strings are null-terminated and allow empty input.

--- benchmarks/oopsla25/stalagmite/template/parser.c
+++ benchmarks/oopsla25/stalagmite/template/parser.c
@@ -8,18 +8,12 @@
 #include "__SUBJECT__.c"
 
 int main(int argc, char *argv[]) {
-  char my_string[10240];
+  char my_string[10240] = {0};
   if (argc == 1) {
     int chars = read(fileno(stdin), my_string, 10240);
-    if (!chars) {
-      exit(1);
-    }
   } else {
     int fd = open(argv[1], O_RDONLY);
-    int chars = read(fd, my_string, 10240);
-    if (!chars) {
-      exit(3);
-    } 
+    int chars = read(fd, my_string, 10240);    
     close(fd);
   }
   printf("val: <%s>\n", my_string);
