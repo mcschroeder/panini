0aa5cc10e7b1314cf368c2fe4676fb260af44212
Michael Schröder <michael.schroeder@tuwien.ac.at>
2025-02-28 15:35:23 +0100

    Fix test case

--- benchmarks/oopsla25/subjects/233.c
+++ benchmarks/oopsla25/subjects/233.c
@@ -7,7 +7,7 @@
 
 void f233(const char* s) {
     int i = 0;
-    assert(strchr(s + i, 'b') == s + i);
+    assert(strchr(s + i, 'a') == s + i);
     i++;
     while (i < strlen(s)) {
         assert(strchr(s + i, 'b') == s + i);
--- tests/eval_py/233_abS.py
+++ tests/eval_py/233_abS.py
@@ -1,6 +1,6 @@
 def f233(s: str):
   i = 0
-  assert s.index("b",i) == i
+  assert s.index("a",i) == i
   i += 1
   while i < len(s):
     assert s.index("b", i) == i
