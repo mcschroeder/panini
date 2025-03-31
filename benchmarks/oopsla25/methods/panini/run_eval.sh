89af4c82c3cf239298a869592945034a176026d5
Michael Schröder <michael.schroeder@tuwien.ac.at>
2025-03-31 17:36:00 +0200

    WIP eval smt

--- benchmarks/oopsla25/methods/panini/run_eval.sh
+++ benchmarks/oopsla25/methods/panini/run_eval.sh
@@ -3,8 +3,8 @@
 # NOTE: this is intended to be run from within the Panini Docker container
 # see README for more information
 
-subjects_dir="/benchmark/subjects"
-results_dir="/benchmark/methods/panini/results"
+subjects_dir="../../subjects"
+results_dir="../../methods/panini/results"
 results_table="$results_dir/results.csv"
 
 #cabal build panini
@@ -12,10 +12,10 @@ results_table="$results_dir/results.csv"
 
 mkdir -p $results_dir
 
-echo "subject,time_ms,status" > $results_table
+echo "subject,time_ms,status,sat" > $results_table
 
-printf "%-10s %-10s %-8s %-30s\n" "Subject" "Time (ms)" "Status" "Regex"
-printf "%-10s %-10s %-8s %-30s\n" "----------" "----------" "--------" "--------------------"
+printf "%-10s %-10s %-8s %-8s %-30s\n" "Subject" "Time (ms)" "Status" "Verified" "Regex" 
+printf "%-10s %-10s %-8s %-8s %-30s\n" "----------" "----------" "--------" "--------" "--------------------"
 
 for file in "$subjects_dir"/*.py; do
   if [ -e "${file%.py}.pan" ]; then
@@ -27,10 +27,10 @@ for file in "$subjects_dir"/*.py; do
 
   printf "%-10s " $subject
 
-  start_time=$(date +%s%3N)
-  cabal run -v0 panini -- --no-color "$file" > $outfile 2>&1
+  start_time=$(date +%s)
+  cabal run -v0 panini -- --no-color "$file" --dump-smt > $outfile 2>&1
   exit_code=$?
-  end_time=$(date +%s%3N)
+  end_time=$(date +%s)
   elapsed_time=$((end_time - start_time))
 
   echo "$subject,$elapsed_time,$exit_code" >> $results_table
@@ -38,16 +38,31 @@ for file in "$subjects_dir"/*.py; do
   printf "%-10s %-8s " $elapsed_time $exit_code
 
   if [ "$exit_code" -eq 0 ]; then
-    sed -n '$ s/.*∈ \(.*\)}.*/\1/p; s/.*= "\(.*\)".*/\1/p; s/.*: (s:𝕊) → .*/\.\*/p; s/.*: {s:𝕊 | false} → .*/\[\]/p' $outfile | head -n 1 | sed -z 's/\n$//' > $results_dir/$subject.regex
-    cat $results_dir/$subject.regex
-    echo
-    
-    if [ "$(cat $results_dir/$subject.regex)" == "[]" ]; then
-      echo '{"<start>":[]}' > "$results_dir/$subject.grammar"
+
+    if grep -q "UNVERIFIED" $outfile; then
+      echo -n ",0" >> $results_table
+      printf "%-8s" "0"
     else
-      cabal run -v0 regex -- -f posix -t fuzzingbook -o "$results_dir/$subject.grammar" "$results_dir/$subject.regex"
+      echo -n ",1" >> $results_table
+      printf "%-8s" "1"
     fi
+
+    sed -n '$ s/.*∈ \(.*\)}.*/\1/p; s/.*= "\(.*\)".*/\1/p; s/.*: (s:𝕊) → .*/\.\*/p; s/.*: {s:𝕊 | false} → .*/\[\]/p' $outfile | head -n 1  > $results_dir/$subject.regex
+    cat $results_dir/$subject.regex
+
+
+    
+  #   if [ "$(cat $results_dir/$subject.regex)" == "[]" ]; then
+  #     echo '{"<start>":[]}' > "$results_dir/$subject.grammar"
+  #   else
+  #     cabal run -v0 regex -- -f posix -t fuzzingbook -o "$results_dir/$subject.grammar" "$results_dir/$subject.regex"
+  #   fi
   else
     printf "\n"
   fi  
+
+  if [ -e "$subjects_dir/$subject.smt" ]; then
+    mv "$subjects_dir/$subject.smt" "$results_dir/$subject.smt"
+  fi
+
 done
