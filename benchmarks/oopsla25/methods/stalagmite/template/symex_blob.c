497a621667c6dea3ed2e19f41c5f3c4fbee42fb9
Michael Schröder <michael.schroeder@tuwien.ac.at>
2025-06-30 18:44:59 +0200

    Fix Stalagmite evaluation
    Thanks to Leon Bettscheider <leon.bettscheider@cispa.de> for this patch!

---
+++ benchmarks/oopsla25/methods/stalagmite/template/symex_blob.c
@@ -0,0 +1,13 @@
+int kw_ep(int argc, char* argv[]) {
+  int SYMEX_SIZE = atoi(argv[1]);
+  char* inp = malloc(SYMEX_SIZE);
+  klee_make_symbolic(inp, SYMEX_SIZE, "input_str");
+  // restrict possible inputs to printable ASCII
+  for (int i = 0; i < SYMEX_SIZE; i++) {
+	  klee_assume((inp[i] == '\0') | (inp[i] >= 32 & inp[i] < 127));
+  }
+  klee_assume(inp[SYMEX_SIZE - 1] == '\0');
+  __SUBJECT_FUNC__(inp);
+
+  return 0;
+}
